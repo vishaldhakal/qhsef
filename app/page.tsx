e4656fa1e45746dc9a826ac440d6d6c58f501c70
kapil dev sapkota <kapildevsapkota98@gmail.com>
2025-01-17 16:13:10 +0545

    fghfhg

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import LandingPage from "./landingPage/page";
+import Header from "@/components/layout/header/header";
+import Hero from "@/components/hero /landingpage";
+import About from "@/components/hero /about";
+import { Objective } from "@/components/hero /objective";
+import { Advantage } from "@/components/hero /advantage";
+import Contact from "@/components/hero /contact";
+import { Footer } from "@/components/layout/footer/footer";
 
 export default function Home() {
   return (
     <>
-      <LandingPage />
+      <Header />
+      <Hero />
+      <About />
+      <Objective />
+      <Advantage />
+      <Contact />
+      <Footer />
     </>
   );
 }
